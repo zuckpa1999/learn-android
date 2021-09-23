6d94d365ed7a8f94cb736d1cc8c75b1b9985e249
Barameerak Koonmongkon <Barameerak.Koonmongkon@ibm.com>
2021-09-23 15:42:09 +0700

    added a button to get a random age

--- app/src/main/java/com/example/android_demos/CatDetail.kt
+++ app/src/main/java/com/example/android_demos/CatDetail.kt
@@ -3,6 +3,7 @@ package com.example.android_demos
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -20,12 +21,14 @@ class CatDetail : AppCompatActivity() {
         val receivedCatOrigin = intent.getStringExtra("cat_origin")
         val receivedCatBodyType = intent.getStringExtra("cat_body_type")
         val imageId = intent.extras!!.getInt("cat_image")
-
+        val cat_age = 4
         // get the ref for their view
         val catBreed = findViewById<TextView>(R.id.cat_breed)
         val catOrigin = findViewById<TextView>(R.id.cat_origin)
         val catBodyType = findViewById<TextView>(R.id.cat_body_type)
+        val catAge = findViewById<TextView>(R.id.cat_age)
         val imageView = findViewById<ImageView>(R.id.image)
+        val btn = findViewById<Button>(R.id.button)
 
 
         // hook them up
@@ -33,6 +36,11 @@ class CatDetail : AppCompatActivity() {
         catBreed.text = "Breed: ${receivedCatBreed}"
         catOrigin.text = "Origin: ${receivedCatOrigin}"
         catBodyType.text = "Body type: ${receivedCatBodyType}"
+        catAge.text = "Age: ${cat_age}"
+
+        btn.setOnClickListener {
+            catAge.text = "Age: ${(0..10).random() }"
+        }
 
 
     }
